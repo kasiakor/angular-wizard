13f6aa936fe8aca9473524758f68365bfb48fdb8
kasia.kordys <kasia.kordys@gmail.com>
2022-02-28 19:51:34 +0200

    implement life cycle hook interface

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { IClass } from './class.model';
 import { CatalogRepositoryService } from "./catalog-repository.service";
@@ -9,7 +9,7 @@ import { FilterClassesService } from './filter-classes.service';
   styleUrls: ['./catalog.component.css'],
   templateUrl: './catalog.component.html'
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
   classes:IClass[] = [];
   visibleClasses:IClass[] =[];
 
