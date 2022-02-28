7686c17b69caf6f58a961d9df7004c6c3058e68a
kasia.kordys <kasia.kordys@gmail.com>
2022-02-28 14:02:27 +0200

    add feature module for catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 
 import { appRoutes } from './routes'
 import { AppComponent }  from './app.component';
-import { CatalogComponent } from "./catalog/catalog.component";
 import { RegisterComponent } from "./users/register.component";
 import { SignInComponent } from "./users/sign-in.component";
-import { CatalogRepositoryService } from "./catalog/catalog-repository.service";
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { CatalogModule } from './catalog/catalog.module';
 
 @NgModule({
   imports:      [
@@ -19,15 +18,15 @@ import { SharedModule } from './shared/shared.module';
     ReactiveFormsModule,
     CoreModule,
     SharedModule,
+    CatalogModule,
     RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     AppComponent,
-    CatalogComponent,
     RegisterComponent,
     SignInComponent
   ],
-  providers: [ CatalogRepositoryService ],
+  providers: [ ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
