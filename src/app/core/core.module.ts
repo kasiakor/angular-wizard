06368c0803e47b97cdd83e37d52fff2785681967
kasia.kordys <kasia.kordys@gmail.com>
2022-02-28 12:55:47 +0200

    create shared module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterModule } from '@angular/router';
+
 import { UserRepositoryService } from './user-repository.service';
 import { NavBarComponent } from './nav-bar.component';
 import { AccountMenuComponent } from './account-menu.component';
