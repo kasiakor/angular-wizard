a5612fd76f14486d85eb82a8656907433feb4b21
kasia.kordys <kasia.kordys@gmail.com>
2022-03-01 16:51:11 +0200

    add pipe

--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
 
 import { SharedModule } from '../shared/shared.module';
 import { CatalogComponent } from "./catalog.component";
 import { CatalogRepositoryService } from "./catalog-repository.service";
 import { FilterClassesService } from "./filter-classes.service";
+import { OrderByPipe } from "./order-by.pipe";
 
 
 @NgModule({
-    imports:[ RouterModule, SharedModule ],
-    declarations:[ CatalogComponent ],
-    exports:[],
+    imports:[ CommonModule, RouterModule, SharedModule ],
+    declarations:[ CatalogComponent, OrderByPipe ],
+    exports:[ CatalogComponent ],
     providers:[ CatalogRepositoryService, FilterClassesService ]
 })
 
